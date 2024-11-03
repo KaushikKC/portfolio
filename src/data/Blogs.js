d71fbc3587ee18431f35225cd2ac559b7c022fe9
MadhuVarshaP <madhuvarsha0608@gmail.com>
2024-11-03 21:43:30 +0530

    new blog updated

--- src/data/Blogs.js
+++ src/data/Blogs.js
@@ -4,7 +4,7 @@ const blogs = [
     title: "Rollups Made Blockchain Simpler",
     description:
       "First What is a rollup? A rollup is a Layer-2 scaling solution that improves blockchain throughput by bundling multiple transactions and submitting them as a single transaction on the main chain.🚀",
-    url: "https://mirror.xyz/0x5BDf4cE6749d7e93c05897fa23871C280BF59b5b/x6E53WHF7N31LbFO--c8MkkXnBnKE226mmNzsLkT5x4",
+    url: "https://medium.com/@kccreations1704/rollups-made-blockchain-simpler-70eec42f2dd3",
   },
   {
     id: 2,
@@ -12,14 +12,28 @@ const blogs = [
       "Bridging Between Layer 1 and Rollups: Why It Matters and How It Works",
     description:
       "Ever seen a blockchain take a shortcut? Yep, thats a bridge. 🛣️",
-    url: "https://mirror.xyz/0x5BDf4cE6749d7e93c05897fa23871C280BF59b5b/YiAqEQ7SUFgs8gecy1aP1UqJlXhaQK9ELQNo_c3Qpw8",
+    url: "https://medium.com/@kccreations1704/bridging-between-layer-1-and-rollups-why-it-matters-and-how-it-work-a63d2f4b88b9",
   },
   {
     id: 3,
     title:
+      "Building a Decentralized Tic Tac Toe Game with Stackr Labs",
+    description: "Let's create a decentralized Tic Tac Toe game using Stackr Labs and Micro Rollups (MRUs).",
+    url: "https://medium.com/@kccreations1704/building-a-decentralized-tic-tac-toe-game-with-stackr-labs-f7e383b35bd9",
+  },
+  {
+    id: 4,
+    title:
       "What is Data Availability (DA) and Why is it Important for Rollups?",
     description: "Data Availability (DA) & Rollups—What’s the Connection?",
-    url: "https://mirror.xyz/0x5BDf4cE6749d7e93c05897fa23871C280BF59b5b/7Vx-hGcGOJyt1goib2MyuHcEjxaHt9ce4IzRA5RQYJE",
+    url: "https://medium.com/@kccreations1704/what-is-data-availability-da-and-why-is-it-important-for-rollups-ba78a6107c74",
+  },
+  {
+    id: 5,
+    title:
+      "How Do Blockchains Ensure All Data Is Available Without Downloading Everything? Exploring Data Availability Sampling (DAS)",
+    description: "Have you ever wondered how blockchains can scale up without forcing everyone to download every bit of data?",
+    url: "https://medium.com/@kccreations1704/how-do-blockchains-ensure-all-data-is-available-without-downloading-everything-78b5b5b9e7a6",
   },
 ];
 
